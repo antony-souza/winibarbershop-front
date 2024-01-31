f3fcef3a6c9f1edb88d4a7cd416385656b9d38f2
antony-souza <antonygustavo09@hotmail.com>
2024-01-31 07:53:24 -0300

    test

--- src/reset-pass/main.js
+++ src/reset-pass/main.js
@@ -2,12 +2,10 @@ const formReset = document.getElementById('form-reset');
 formReset.addEventListener('submit', e =>{
     e.preventDefault()
     
-    const email = document.getElementById('token-reset').value;
-    const token = document.getElementById('token-reset').value;
-    const newPass = document.getElementById('newPassword-reset').value;
+    const email = document.getElementById('email-login').value;
 
     fetch('http://localhost:8100/users/reset',{
-        method:'GET',
+        method:'POST',
         mode:'cors',
         headers:{
             'Content-Type':'application/json'
@@ -18,6 +16,7 @@ formReset.addEventListener('submit', e =>{
     })
     .then((response) => response.json())
     .then((data) => {
+        document.getElementById('email-login').value = '';
      console.log(data);
 
 })})
