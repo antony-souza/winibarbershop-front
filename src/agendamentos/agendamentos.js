fc9a840db9baa06ca3cea4b01e08056615e366f6
antony-souza <antonygustavo10202016@gmail.com>
2024-03-27 20:04:23 -0300

    agendamentos

--- src/agendamentos/agendamentos.js
+++ src/agendamentos/agendamentos.js
@@ -3,25 +3,36 @@ const formAgendamento = document.getElementById('agendamentoForm');
 formAgendamento.addEventListener('submit', e => {
     e.preventDefault();
 
-    const dataHour = document.getElementById('dataHora').value; // Corrigido o ID para 'dataHora'
-    const barbeiro = document.getElementById('barbeiro').value;
+    const name = document.getElementById('client').value;
+    const dateHour = document.getElementById('dataHora').value;
+    const employee = document.getElementById('barbeiro').value;
 
-    fetch('http://localhost:8100/agendar', { // Removido o espaço em branco após a URL
+    const token = localStorage.getItem('token');
+
+    fetch('http://localhost:8100/Schedule', {
         method: 'POST',
         mode: 'cors',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
         },
         body: JSON.stringify({
-            dataHour: dataHour,
-            barbeiro: barbeiro
+            client: name,
+            dateHour: dateHour,
+            employee: employee
         })
     })
     .then(response => response.json())
-    .then(data => {
+    .then((data) => {
+       
+        document.getElementById('client').value = '';
+        document.getElementById('dataHora').value = '';
+        document.getElementById('barbeiro').value = '';
+
         console.log(data);
     })
     .catch(error => {
-        console.error('Erro ao enviar o formulário:', error);
+        console.error('Erro:', error);
+        alert('Ocorreu um erro ao processar de agendamento');
     });
 });
